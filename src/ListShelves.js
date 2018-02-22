8daf66ab72313abf3ede7797fadb2651b22dfb7c
mka <mka281@icloud.com>
2018-02-23 01:33:58 +0300

    Turn add book anchor tag to Link component

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
 class ListShelves extends Component {
@@ -22,7 +23,9 @@ class ListShelves extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a>Add a book</a>
+          <Link to="/search">
+          Add a book
+          </Link>
         </div>
       </div>
     )
