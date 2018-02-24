f44862548578ce71a30c417724201d41782555bf
mka <mka281@icloud.com>
2018-02-24 22:40:59 +0300

    Move BooksAPI search function to SearchBook component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,14 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListShelves from './ListShelves'
 import SearchBook from './SearchBook'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    query:"art",
-    books:[]
-  }
-
-  componentDidMount() {
-    BooksAPI.search(this.state.query)
-    .then((books) => {
-      this.setState({ books })
-      console.log(this.state)
-    })
-  }
-
   render() {
     return (
       <div className="app">
         <Route exact path="/" component={ListShelves}/>
-        <Route path="/search" render={() => (
-        (<SearchBook
-          books={this.state.books}
-        />)
-        )}/>
+        <Route path="/search" component={SearchBook}/>
       </div>
     )
   }
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBook extends Component {
 
   state = {
-    query: "react"
+    query: "react",
+    books: []
   }
 
   updateQuery = (query) => {
     this.setState({
       query: query.trim()
+    })
+  }
+
+  componentDidMount() {
+    BooksAPI.search(this.state.query)
+    .then((books) => {
+      this.setState({ books })
+      console.log(books)
     })
   }
 
@@ -35,7 +45,7 @@ class SearchBook extends Component {
           </div>
           <div className="search-books-results">
             <ol className="books-grid">
-              {this.props.books.map(book => (
+              {this.state.books.map(book => (
                   <li key={book.id}>
                     <Book
                       title={book.title}
