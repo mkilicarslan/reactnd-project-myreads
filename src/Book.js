4c3f35037309c4ce4624752f0c87eeabd267c003
mka <mka281@icloud.com>
2018-02-26 15:49:27 +0300

    Handle books with no image or no author

--- src/Book.js
+++ src/Book.js
@@ -4,17 +4,23 @@ import BookshelfChanger from './BookshelfChanger'
 class Book extends Component {
   render() {
     const { book, changeShelf } = this.props
+    const defaultImg = book.imageLinks ? book.imageLinks.smallThumbnail : ""
+    const defaultAuthor = book.authors ? book.authors : "not found"
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-cover" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${defaultImg})` }}>
+          </div>
           <BookshelfChanger
             book={book}
             changeShelf={changeShelf}
           />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-authors">{defaultAuthor}</div>
       </div>
     )
   }
